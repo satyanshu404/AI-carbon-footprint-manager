78e8e957c751fa993cc4f9e17cdd4b843fb0d81d
Satyanshu kumar <67653823+satyanshu404@users.noreply.github.com>
2024-06-20 22:47:05 +0530

    [major] updated tools desc and added code generator tool

--- tools/tools.py
+++ tools/tools.py
@@ -6,13 +6,14 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dotenv import load_dotenv
 import tools.utils as utils
+import constants
 from langchain.agents import tool
 
 load_dotenv()
 
 @tool
 def read_files(file_paths:list[str]) -> list[str] | str:
-    '''Reads the content of the files at the provided paths and returns the content as a list of strings.'''
+    '''Reads the content files from the provided file paths and returns the content as a list of strings.'''
     try:
         print("Reading files...")
         read_files = utils.ReadFiles()
@@ -22,10 +23,10 @@ def read_files(file_paths:list[str]) -> list[str] | str:
     
 @tool
 def google_search(query: str) -> str:
-    '''Searches the web for the provided query and returns the search results.'''
+    '''Searches the web for the provided query and returns the links of the related web pages.'''
     try:
         print("Searching the web...")
-        return utils.GoogleSearch().search(query)
+        return utils.GoogleSearch().search(query, restult_count=constants.GoogleSearchConstants.NUMBEROFSEARCHS)
     except Exception as e:
         return f"An error occurred while searching the web: {str(e)}"
     
@@ -37,3 +38,17 @@ def get_image_content(image_path: str) -> str:
         return utils.ReadImage().read_image(image_path)
     except Exception as e:
         return f"An error occurred while reading the image content: {str(e)}"
+    
+@tool
+def code_generator_and_executer(prompts:str, function_name: str, *args):
+    '''Executes the code provided in the prompts and returns the result.
+    Arguments:
+    prompts: str - A detailed prompt with informations and insturctions for generating the code.
+    function_name: str - The name of the function to execute.
+    *args: Any - The arguments to pass to the function.
+    '''
+    try:
+        print("Executing code...")
+        return utils.CodeExecuter().execute_code(prompts, function_name, *args)
+    except Exception as e:
+        return f"An error occurred while executing the code: {str(e)}"
