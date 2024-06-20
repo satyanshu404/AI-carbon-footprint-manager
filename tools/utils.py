49e4df40175b8510b3a3a236eba6df807062e0a0
Satyanshu kumar <67653823+satyanshu404@users.noreply.github.com>
2024-06-20 11:23:11 +0530

    [minor] added google search

--- tools/utils.py
+++ tools/utils.py
@@ -4,6 +4,7 @@ import os
 # Add the root directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import re
 import pandas as pd
 import PyPDF2
 import docx
@@ -11,18 +12,17 @@ import json
 from bs4 import BeautifulSoup
 from openai import OpenAI
 import base64
-from enum import Enum
-from io import BytesIO, StringIO
+import requests
 from typing import Union
 from constants import Constants
 import models
 from PIL import Image
 from dotenv import load_dotenv
-from langchain.agents import tool
+
 
 load_dotenv()
 
-class ReadFiles():
+class ReadFiles:
     '''Reads the content of a file based on its extension and returns the content as a string or a dataframe.'''
     def __init__(self):
         pass
@@ -80,7 +80,7 @@ class ReadFiles():
             soup = BeautifulSoup(file, 'html.parser')
             return soup.get_text()
         
-class ReadImage():
+class ReadImage:
     def __init__(self, model_name:str = Constants().MODEL_NAME):
         self.model_name:str = model_name
         self.client = OpenAI()
@@ -111,4 +111,69 @@ class ReadImage():
         except Exception as e:
             return f"A error occurred while reading the image: {str(e)}"
 
+class GoogleSearch:
+    def __init__(self):
+        self.key = os.getenv('GOOGLE_API_KEY')
+        self.cx = os.getenv('GOOGLE_SEARCH_ENGINE_ID')
+        self.url = Constants.GOOGLE_SEARCH_URL
+        self.save_file_path = Constants.GOOGLE_SEARCH_RESULTS_PATH
 
+    @staticmethod
+    def build_payloads(self, 
+                       query: str, 
+                       start:int=1, 
+                       num:int=10,
+                       **params)->dict:
+        ''' Build the payloads for the Google Search API'''
+
+        payload = {
+            'key': self.key,
+            'cx': self.cx,
+            'q': query,
+            'start': start,
+            'num': num,
+        }
+        payload.update(params)
+        return payload
+
+    @staticmethod
+    def make_request(self, payload: dict) -> dict:
+        '''Make a request to the Google Search API'''
+        response = requests.get(self.url, params=payload)
+        return response.json()
+
+    def clean_file_name(self, file_name:str)->str:
+        '''Clean the file name'''
+        # remove special characters
+        file_name = file_name.lower()
+        return re.sub(r'[^\w\s-]', '', file_name).strip().replace(' ', '_')
+
+    def search(self,
+               query:str,
+               restult_count:int=10)->str:
+        '''Search Google and return status of the search'''
+        items = []
+        reminder = restult_count%10
+        if reminder > 0:
+            pages = restult_count//10 + 1
+        else:
+            pages = restult_count//10 
+
+        try:
+            for i in range(pages):
+                if pages == i+1 and reminder > 0:
+                    payload = self.build_payloads(query, start=(i+1)*10, num=reminder)
+                else:
+                    payload = self.build_payloads(query, start=(i+1)*10)
+                # print(payload)
+                response = self.make_request(payload)
+                # print(response)
+                items.extend(response['items'])
+
+            query_string_cleaned = self.clean_file_name(query)
+            df = pd.json_normalize(items)
+            save_file_path = '{0}/Search_resutls_{1}.csv'.format(self.save_file_path, query_string_cleaned)
+            df.to_csv(save_file_path, index=False)
+            return f'''Search results saved in {save_file_path}'''
+        except Exception as e:
+            return f'''Error occured during search: {e}'''
